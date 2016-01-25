269ccec2529d140b219551aa52a3f4c0be995a56
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-25 16:57:49 -0600

    Done

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,5 +2,11 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-  
+  numbers = phone_number_str.chars
+  numbers.delete(" ")
+  numbers.delete("-")
+  numbers.delete(".")
+  return "(" + numbers[0..2].join + ")" + " " + numbers[3..5].join + "-" + numbers[6..9].join
 end
+
+puts format_phone_number('1235557890')
